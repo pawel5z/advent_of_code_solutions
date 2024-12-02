3620d35e3d561dc762e1f633b52061f2ea9b6b0a
Paweł Szmergała <pawelszme@gmail.com>
2024-12-02 20:47:57 +0100

    Add day 2 solutions

---
+++ day02/part2.py
@@ -0,0 +1,50 @@
+from typing import Iterable, Callable
+
+
+def is_monotonic_and_safe[T](less_operator: Callable[[T, T], bool], sequence: Iterable[T]) -> bool:
+    previous = None
+    for element in sequence:
+        if previous is None:
+            previous = element
+            continue
+
+        delta = abs(previous - element)
+        if not less_operator(previous, element) or delta < 1 or delta > 3:
+            return False
+
+        previous = element
+
+    return True
+
+
+def is_monotonic_and_safe_dampened[T](less_operator: Callable[[T, T], bool], sequence: Iterable[T]) -> bool:
+    n = len(sequence)
+
+    if n == 0:
+        return True
+
+    for i in range(len(sequence)):
+        if i == n:
+            if is_monotonic_and_safe(less_operator, sequence[:i]):
+                return True
+        elif is_monotonic_and_safe(less_operator, sequence[:i] + sequence[i + 1:]):
+            return True
+
+    return False
+
+
+if __name__ == "__main__":
+    safe_reports_count = 0
+
+    while True:
+
+        try:
+            report = list(map(int, input().split()))
+        except EOFError:
+            break
+
+        if is_monotonic_and_safe_dampened(lambda a, b: a < b, report) \
+                or is_monotonic_and_safe_dampened(lambda a, b: a > b, report):
+            safe_reports_count += 1
+
+    print(safe_reports_count)
