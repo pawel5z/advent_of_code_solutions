b7a1c6e6f384ef7dbe6969aa59bc9e310e7fa66c
Paweł Szmergała <pawelszme@gmail.com>
2024-12-29 19:53:11 +0100

    wip day 17 part 2

---
+++ day17/rev.py
@@ -0,0 +1,59 @@
+from typing import List, Dict, Set, Tuple
+import sys
+
+
+def run_equiv_puzzle_machine(a: int) -> List[int]:
+    """Equivalent of the puzzle machine.
+    """
+    result: List[int] = []
+
+    digit = a % 8
+    result.append((digit ^ 5 ^ (a // 2 ** (digit ^ 1))) % 8)
+    a //= 8
+
+    while a > 0:
+        digit = a % 8
+        result.append((digit ^ 5 ^ (a // 2 ** (digit ^ 1))) % 8)
+        a //= 8
+
+    return result
+
+
+if __name__ == '__main__':
+    puzzle_output_str: str = '2,4,1,1,7,5,1,4,0,3,4,5,5,5,3,0'
+    puzzle_output_list: List[int] = list(map(int, puzzle_output_str.split(',')))
+    rev_output = list(reversed(puzzle_output_list))
+    # print(target_output)
+
+    # print(run_equiv_puzzle_machine(42))
+    # sys.exit()
+
+    a = 0
+
+    for i, output_digit in enumerate(rev_output):
+        ok = False
+
+        for d in range(8):
+            if ok:
+                break
+
+            divisor = 2 ** (d ^ 1)
+
+            for remainder in range(divisor):
+                # a_cand = (a << 3 | d)
+                a_cand = (a << 3 | d) + remainder
+                # a_cand = a * divisor + remainder
+                # a_cand = (a << 3) + remainder
+                # a_cand = a + remainder
+                partial_output = run_equiv_puzzle_machine(a_cand)
+                output_fragment = puzzle_output_list[len(puzzle_output_list) - i - 1:]
+
+                if (d ^ 5 ^ (a_cand // divisor)) % 8 == output_digit and partial_output == output_fragment:
+                    a = a_cand
+                    ok = True
+                    break
+
+        if not ok:
+            raise ValueError
+
+    print(a)
