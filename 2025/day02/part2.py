a39232563309c6bbb8f5c0b654a836f482b7b351
Paweł Szmergała <pawelszme@gmail.com>
2025-12-02 20:29:02 +0100

    Add year 2025 day 2 solutions

---
+++ 2025/day02/part2.py
@@ -0,0 +1,10 @@
+import re
+
+if __name__ == "__main__":
+    total = 0
+    for b, e in map(lambda e: tuple(map(int, e.split("-"))), input().split(",")):
+        for n in range(b, e+1):
+            if re.match(r"^(\d+)\1+$", str(n)):
+                # print(n)
+                total += n
+    print(total)
