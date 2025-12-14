7afd7701e7a7ce0a24a64804777d4183214227c1
Paweł Szmergała <pawelszme@gmail.com>
2025-12-14 17:17:08 +0100

    Attempt A*

--- 2025/day10/part2.py
+++ 2025/day10/part2.py
@@ -2,6 +2,11 @@ import sys
 from functools import reduce
 from tqdm import tqdm
 from copy import deepcopy
+import heapq
+
+
+def h(node: tuple[int], dest: tuple[int]) -> int:
+    return sum((dest[i] - node[i] for i in range(len(dest))))
 
 
 def transit(node: tuple[int], transition: list[int]) -> list[int]:
@@ -17,21 +22,22 @@ def neighbours(node: tuple[int], transitions: list[list[int]]) -> list[tuple[int
 
 def find_shortest_path_length(dest: tuple[int], transitions: list[list[int]]) -> int:
     source = (0,) * len(dest)
-    queue: list[tuple[int]] = [source]
-    to_visit: set[tuple[int]] = {source}
+    queue: list[tuple[int, tuple[int]]] = [(0, source)]
+    heapq.heapify(queue)
+    marked_visit: set[tuple[int]] = {source}
     distance: dict[tuple[int], int] = {source: 0}
     while queue:
-        current = queue.pop(0)
+        _, current = heapq.heappop(queue)
         for neighbour in neighbours(current, transitions):
-            if neighbour in to_visit:
+            if neighbour in marked_visit:
                 continue
-            to_visit.add(neighbour)
+            marked_visit.add(neighbour)
             if any((neighbour[i] > dest[i]) for i in range(len(dest))):
                 continue
             distance[neighbour] = distance[current] + 1
             if neighbour == dest:
                 break
-            queue.append(neighbour)
+            heapq.heappush(queue, (distance[neighbour] + h(neighbour, dest), neighbour))
     return distance[dest]
 
 
