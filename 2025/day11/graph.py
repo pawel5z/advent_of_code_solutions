5137a8e2fa14982cb7189c302a5d0f17521861c2
Paweł Szmergała <pawelszme@gmail.com>
2025-12-19 20:47:06 +0100

    Extract path counting method with path count merging into a graph module

---
+++ 2025/day11/graph.py
@@ -0,0 +1,13 @@
+def count_paths(src: str, dst: str, neighbors: dict[str, set[str]]) -> int:
+    path_counts: dict[str, int] = {src: 1}
+    queue: list[str] = [src]
+
+    while queue:
+        current = queue.pop(0)
+        for neighbor in neighbors[current]:
+            if neighbor not in path_counts:
+                queue.append(neighbor)
+                path_counts[neighbor] = 0
+            path_counts[neighbor] += path_counts[current]
+
+    return path_counts[dst] if dst in path_counts else 0
